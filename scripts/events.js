9c314e6aa109e0a574d012e1ae34a83c8a0e76a2
Tatu Laras <tatu.hauki@gmail.com>
2021-08-23 00:27:45 +0300

    Started on game

--- scripts/events.js
+++ scripts/events.js
@@ -9,25 +9,47 @@ const playSound = (keycode) => {
     osc.stop(context.currentTime + 0.03); // stop 2 seconds after the current time
 };
 
+const buttonDown = (id) => {
+    var key = document.getElementById(id);
+    key.classList.add('active');
+    key.classList.remove('fade');
+    playSound(id);
+    setTimeout(() => {
+        key.classList.add('fade');
+    }, 10);
+
+    if (key.classList.contains('num-slot')) {
+        for (var i = 0; i <= 9; i++) {
+            if (key.classList.contains('slot-' + i)) {
+                changeNumbarItem(i);
+                break;
+            }
+        }
+    }
+};
+
+document.querySelectorAll('.key').forEach((k) => {
+    k.addEventListener(
+        'mousedown',
+        (event) => {
+            buttonDown(event.target.id);
+        },
+        false
+    );
+});
+
+document.addEventListener(
+    'mouseup',
+    (event) => {
+        document.querySelectorAll('.key').forEach((k) => k.classList.remove('active'));
+    },
+    false
+);
+
 document.addEventListener(
     'keydown',
     (event) => {
-        var key = document.getElementById(event.keyCode);
-        key.classList.add('active');
-        key.classList.remove('fade');
-        playSound(event.keyCode);
-        setTimeout(() => {
-            key.classList.add('fade');
-        }, 10);
-
-        if (key.classList.contains('num-slot')) {
-            for (var i = 0; i <= 9; i++) {
-                if (key.classList.contains('slot-' + i)) {
-                    changeNumbarItem(i);
-                    break;
-                }
-            }
-        }
+        buttonDown(event.keyCode);
     },
     false
 );
@@ -35,8 +57,11 @@ document.addEventListener(
 document.addEventListener(
     'keyup',
     (event) => {
-        var key = document.getElementById(event.keyCode);
-        key.classList.remove('active');
+        document.getElementById(event.keyCode).classList.remove('active');
     },
     false
 );
+
+setInterval(() => {
+    document.getElementById('keyboard').classList.toggle('second-image');
+}, 125);
--- scripts/gamelogic.js
+++ scripts/gamelogic.js
@@ -1,7 +1,18 @@
+// Remember: in the UI 0 is on the right
+const itemInfoTexts = [
+    '<b>Delete items</b>',
+    'Place a <b>production facility</b>',
+    'Place a <b>conveyor belt</b>',
+    'Place an <b>exporter</b>',
+];
 var selectedItem = 1;
+var infoTextEl = document.getElementById('numbar-details');
 
 const changeNumbarItem = (num) => {
     selectedItem = num;
     document.querySelectorAll('.num-slot').forEach((s) => s.classList.remove('selected'));
     document.querySelector('.slot-' + num).classList.add('selected');
+    if (num < itemInfoTexts.length && num >= 0) {
+        infoTextEl.innerHTML = itemInfoTexts[num];
+    }
 };
