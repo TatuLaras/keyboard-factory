16e07aae8a9b486e5837184fd181c017ef0fa58c
Tatu Laras <tatu.hauki@gmail.com>
2021-08-26 16:19:45 +0300

    Mine by typing

--- scripts/gamelogic.js
+++ scripts/gamelogic.js
@@ -12,17 +12,62 @@ var selectedAction = 0;
 var infoTextElm = document.getElementById('numbar-details');
 
 // Todo dict of images
-var sprites = [];
+var sprites = {
+    production: 'res/img/3d/production.png',
+};
 
 const placeProductionFacilityOnKey = (keycode) => {
     var key = document.getElementById(keycode);
     if (key) {
         var item = key.querySelector('.item');
         if (item) {
-            item.src = 'res/img/3d/production.png';
-            item.classList.add('active');
+            item.src = sprites['production'];
+            item.classList.add('in-use');
         }
     }
+};
+
+var mineString = '';
+
+var resourceTypes = ['IRON', 'COPPER'];
+var resourceAmounts = {
+    IRON: 0,
+    COPPER: 0,
+};
+
+// Mine by typing
+const mine = (keycode) => {
+    mineString += String.fromCharCode(keycode);
+
+    var partialMatch = false;
+    var completeMatch = false;
+
+    resourceTypes.forEach((rt) => {
+        if (rt.slice(0, mineString.length) == mineString) {
+            if (rt.length == mineString.length) {
+                completeMatch = true;
+            } else {
+                partialMatch = true;
+            }
+        }
+    });
+
+    if (completeMatch == true) {
+        // Update data
+        resourceAmounts[mineString] += 1;
+        var counter = document.querySelector('#resources #' + mineString.toLowerCase());
+        if (counter)
+            counter.innerHTML =
+                mineString.charAt(0) +
+                mineString.slice(1).toLowerCase() +
+                ': ' +
+                resourceAmounts[mineString];
+
+        mineString = '';
+    }
+
+    // If word not going on any path, erase
+    if (partialMatch == false) mineString = String.fromCharCode(keycode);
 };
 
 // Called from events
@@ -41,6 +86,10 @@ const performSelectedActionForKey = (keycode) => {
         // Place facility
         case 2:
             placeProductionFacilityOnKey(keycode);
+            break;
+
+        case 5:
+            mine(keycode);
             break;
     }
 };
